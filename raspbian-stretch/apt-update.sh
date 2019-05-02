acd86769835d9be7b2d6ebe3b4492e2fee3874b6
jipp <wolfgang.keller@wobilix.de>
2019-05-02 12:04:46 +0200

    update list command

--- raspbian-stretch/apt-update.sh
+++ raspbian-stretch/apt-update.sh
@@ -3,6 +3,7 @@
 sudo mount -o remount,rw /
 sudo mount -o remount,rw /boot
 sudo apt update
-sudo apt list -a --upgradeable
+sudo apt list --upgradeable
+#sudo apt list -a --upgradeable
 sudo mount -o remount,ro /
 sudo mount -o remount,ro /boot
